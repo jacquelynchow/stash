37bce2f090d3b0c6cec095e782f7e37d193d22f3
Alice Huang <aliceshuang3@gmail.com>
2021-03-12 20:34:34 -0500

    add starting point files

---
+++ recommendations-app/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to the STASH App!</Text>
+      <Text>CS 71 Wehar, Spring 2021</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+});

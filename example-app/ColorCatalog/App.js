34844ae6903b149781ec5feb9b18a94a69a3930d
Alice Huang <aliceshuang3@gmail.com>
2021-03-12 20:46:22 -0500

    add credits for example app

--- example-app/ColorCatalog/App.js
+++ example-app/ColorCatalog/App.js
@@ -1,3 +1,7 @@
+// Example Application
+// Code from LinkedIn Learning Course: Learning React Native by Alex Banks 
+// Link to course: https://www.linkedin.com/learning-login/share?forceAccount=false&redirect=https%3A%2F%2Fwww.linkedin.com%2Flearning%2Flearning-react-native%3Ftrk%3Dshare_ent_url%26shareId%3DbjQJNdnDQ7%252Bk1f8ffDSyyA%253D%253D&account=76206914
+
 import React from "react";
 import ColorList from "./components/ColorList";
 import ColorDetails from "./components/ColorDetails";

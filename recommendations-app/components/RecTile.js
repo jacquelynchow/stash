f4d5da772fa397e0ba2b82347f56a4ef476fa966
Lamia Makkar <lmakkar1@swarthmore.edu>
2021-03-25 22:58:42 -0400

    made recommendations clickable with pop up information

--- recommendations-app/components/RecTile.js
+++ recommendations-app/components/RecTile.js
@@ -1,16 +1,40 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, Pressable } from 'react-native';
+import closePopUpButton from '../assets/closePopUpButton.png';
+import Modal from 'react-native-modal';
 
 const RecTile = (props) => {
 
+  const [isModalVisible, setModalVisible] = useState(false);
+  const toggleModal = () => {
+      setModalVisible(!isModalVisible);
+  };
+
     return (
         <View style={styles.item}>
+            <TouchableOpacity activeOpacity={0.25} onPress={toggleModal}>
                 <Image source={{uri: "https://www.jaipuriaschoolpatna.in/wp-content/uploads/2016/11/blank-img.jpg"}} style={styles.recImage}></Image>
                 <Text style={styles.name}>{props.recName}</Text>
                 <Text style={styles.media}>{props.mediaType}</Text>
+            </TouchableOpacity>
 
             {/* Rec Details PopUp */}
-             {/* TODO: Name of group, pod image upload, show current members */}
+            <Modal isVisible={isModalVisible}>
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <Pressable style={[styles.button, styles.buttonClose]}
+                            onPress={toggleModal} >
+                            <Image source={closePopUpButton} style={{width: 30, height: 30}}/>
+                        </Pressable>
+                        <Image source={{uri: "https://www.jaipuriaschoolpatna.in/wp-content/uploads/2016/11/blank-img.jpg"}} style={styles.recImagePopUp}></Image>
+                        <Text style={styles.modalTitle}> {props.recName} </Text>
+                        <Text style={styles.modalText}> {props.mediaType} </Text>
+                        <Text style={styles.modalText}>Sent by: [username] in {props.groupName} pod</Text>
+                        {/* TODO: change image to be the one they uploaded, show sender instead of [username]*/}
+                    </View>
+                </View>
+            </Modal>
+
         </View>
     )
 }
@@ -31,6 +55,45 @@ const styles = StyleSheet.create({
       // android
       elevation: 2,
     },
+    centeredView: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: 22,
+    },
+    modalView: {
+        margin: 20,
+        backgroundColor: "#D26D64", //TODO when we have conditionals change colour by type
+        borderRadius: 20,
+        padding: 80,
+        alignItems: "center",
+        // ios
+        shadowOffset: {width: 10, height: 10},
+        shadowOpacity: 0.1,
+        shadowRadius: 10,
+        // android
+        elevation: 2,
+    },
+    buttonClose: {
+        position: 'absolute',
+        alignSelf: 'flex-start',
+        marginTop: 10,
+        marginLeft: 10,
+    },
+    modalTitle: {
+        textAlign: "center",
+        fontWeight: 'bold',
+        fontSize: 35,
+        marginTop: 10,
+        marginBottom: 10,
+        color: "white",
+    },
+    modalText: {
+        marginTop: 5,
+        textAlign: "center",
+        fontSize: 13,
+        color: "white"
+    },
     name: {
         fontSize: 20,
         fontWeight: 'bold',
@@ -48,6 +111,11 @@ const styles = StyleSheet.create({
         height: 90,
         marginTop: 20,
         borderRadius: 10,
+    },
+    recImagePopUp:{
+      width: 150,
+      height: 150,
+      borderRadius: 10,
     }
 })
 

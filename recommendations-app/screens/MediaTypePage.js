ae7babd53ab2bc5396f0bbcd53e21a3f4e3a95d3
Leia Rich <lrich1@swarthmore.edu>
2021-04-15 22:30:34 -0400

    deleted uneccessary comments

--- recommendations-app/screens/MediaTypePage.js
+++ recommendations-app/screens/MediaTypePage.js
@@ -1,31 +1,8 @@
 import React, { useEffect, useState,useCallback } from 'react';
 import { StyleSheet, View, ScrollView, TouchableOpacity, Image, Pressable, Text, Button, Alert, Dimensions, RefreshControl} from 'react-native';
 import RecTile from '../components/RecTile';
-import {getMediaRecs, getNumRecsForMedia, getRecs} from '../API/firebaseMethods';
-//will create function that shows the recs from only 1 media type
-//TODO: figure out how to get the mediaType that should be displaying
-{/*
-function showMediaType() {
-  if (props.mediaType == "Article") {
-    return //list of recs with rec_type == "Article"
-  }
-  else if (props.mediaType == "Book") {
-    return //list of recs with rec_type == "Book"
-  }
-  else if (props.mediaType == "Movie") {
-    return //list of recs with rec_type == "Movi"
-  }
-  else if (props.mediaType == "Song") {
-    return //list of recs with rec_type == "Song"
-  }
-  else if (props.mediaType == "TikTok") {
-    return //list of recs with rec_type == "TikTok"
-  }
-  else if (props.mediaType == "Video") {
-    return //list of recs with rec_type == "Video"
-  }
-}
-*/}
+import {getMediaRecs} from '../API/firebaseMethods';
+
 const windowHeight = Dimensions.get('window').height;
 
 export default function MediaTypePage({navigation, route }) {

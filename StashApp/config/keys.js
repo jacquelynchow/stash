0c472a0f3baa3e677bd2a11027b23b5c5df08e25
Lamia Makkar <lmakkar1@swarthmore.edu>
2021-04-23 17:00:33 -0400

    rename

---
+++ StashApp/config/keys.js
@@ -0,0 +1,10 @@
+export default {
+    firebaseConfig: {
+      apiKey: 'AIzaSyCVAt2JFLHCL4rwJYCQbT55FIwCF_K5-18',
+      authDomain: 'stash-app-fc507.firebaseapp.com',
+      projectId: 'stash-app-fc507',
+      storageBucket: 'stash-app-fc507.appspot.com',
+      messagingSenderId: '47351789102',
+      appId: '1:47351789102:web:f77715c4e19e63913351bf'
+    }
+  }

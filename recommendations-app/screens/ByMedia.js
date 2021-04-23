30520576a6bacff3dc9319d4a31ddee78b16b479
lmakkar1 <lmakkar1@swarthmore.edu>
2021-04-22 22:08:29 -0400

    started refresh for page - TODO finish refresh

--- recommendations-app/screens/ByMedia.js
+++ recommendations-app/screens/ByMedia.js
@@ -7,50 +7,81 @@ import songIcon from '../assets/type-icons/song.png';
 import tiktokIcon from '../assets/type-icons/tiktok.png';
 import articleIcon from '../assets/type-icons/article.png';
 import youtubeIcon from '../assets/type-icons/youtube.png';
+import * as firebase from 'firebase';
 import {getMediaRecs } from '../API/firebaseMethods';
 
+//TODO uncomment code to make screen refresh start - need route.params
+const ByMedia = ({navigation, route }) => {
+
+    //console.log(route.params);
+
+    {/*const recData = JSON.parse(JSON.stringify(route.params));
+    const media_Type = recData.media_Type;
+    const [recs, setRecs] = useState([]);
+    useEffect(() => {
+      getMediaRecs(onRecsReceived,media_Type);
+    }, []);
 
 
-const ByMedia = () => {
-   
-    //gets number of recs for specific media type
-    function numRecsForMedia(media_Type) {
-        const [recs, setRecs] = useState([]);
-        useEffect(() => {
-        getMediaRecs(onRecsReceived,media_Type);
-        }, []); 
+    // refresh page function to see new recs
+    const [refreshing, setRefreshing] = useState(false);
+    const onRefresh = useCallback(async () => {
+            setRefreshing(true);
+            await getMediaRecs(onRecsReceived,media_Type) // use await to refresh until function finished
+            .then(() => setRefreshing(false));
+        }, []);*/}
 
-        const onRecsReceived = (recList) => {
+      const onRecsReceived = (recList) => {
         setRecs(recList);
-        };  
-        
-        const numRecs = recs.length;
-        return numRecs;
-    }
-  
-    //gets number of people who sent recs of specific media type
-    function numPeopleForMedia(media_Type) {
-        const [recs, setRecs] = useState([]);
-        useEffect(() => {
-        getMediaRecs(onRecsReceived,media_Type);
-        }, []); 
+      };
 
-        const onRecsReceived = (recList) => {
-        setRecs(recList);
-        }; 
-        const ppl = [];
-        for(var i=0; i<recs.length;i++) {
-            if (!(ppl.includes(recs[i].rec_sender))) {
-                ppl.push(recs[i].rec_sender)
-            }
-        }
-        const numPpl = ppl.length;
-        return numPpl;
-    }
+      //gets number of recs for specific media type
+      function numRecsForMedia(media_Type) {
+          const [recs, setRecs] = useState([]);
+          useEffect(() => {
+          getMediaRecs(onRecsReceived,media_Type);
+          }, []);
+          const onRecsReceived = (recList) => {
+            setRecs(recList);
+            };
+
+          const numRecs = recs.length;
+          return numRecs;
+      }
+
+      //gets number of people who sent recs of specific media type
+      function numPeopleForMedia(media_Type) {
+          const [recs, setRecs] = useState([]);
+          useEffect(() => {
+          getMediaRecs(onRecsReceived,media_Type);
+          }, []);
+
+          const onRecsReceived = (recList) => {
+          setRecs(recList);
+          };
+          const ppl = [];
+          for(var i=0; i<recs.length;i++) {
+              if (!(ppl.includes(recs[i].rec_sender))) {
+                  ppl.push(recs[i].rec_sender)
+              }
+          }
+          const numPpl = ppl.length;
+          return numPpl;
+      }
 
     return (
         <View style={{flex: 1}}>
-             {/* TODO: refresh page */}
+            {/* TODO: refresh page -- not currently working*/}
+            {/* Pull screen down for recommendations refresh */}
+            {/*<ScrollView
+                contentContainerStyle={styles.container}
+                refreshControl={
+                    <RefreshControl
+                      refreshing={refreshing}
+                      onRefresh={onRefresh}
+                    />
+                  }>
+            </ScrollView>*/}
 
             {/* Show various rec types */}
             <ScrollView contentContainerStyle={styles.container}>
